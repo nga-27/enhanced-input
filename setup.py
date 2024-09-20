c8888a654084a2fc457bb1206b72664844b2a658
Nick Amell <namell91@gmail.com>
2024-09-20 12:22:51 -0500

    setting up framework

---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# Based on: https://github.com/kennethreitz/setup.py/blob/master/setup.py
+"""
+Setup tools
+Use setuptools to install package dependencies. Instead of a requirements file you
+can install directly from this file.
+`pip install .`
+You can install dev dependencies by targeting the appropriate key in extras_require
+```
+pip install .[dev] # install requires and test requires
+pip install '.[dev]' # install for MAC OS / zsh
+
+```
+See: https://packaging.python.org/tutorials/installing-packages/#installing-setuptools-extras
+"""
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'enhanced-input'
+DESCRIPTION = 'Python "input" module enhancements, such as combining colorama and updates to johejo "inputimeout" library.'
+URL = 'https://github.mmm.com/nga-27/enhanced-input'
+EMAIL = 'namell91@gmail.com'
+AUTHOR = 'Nick Amell'
+REQUIRES_PYTHON = '>=3.9.0, <3.13.0'
+VERSION = '0.1.0'
+
+# What packages are required for this module to be executed?
+REQUIRES = [
+
+]
+
+REQUIRES_DEV = [
+    "pylint==3.2.5"
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(
+        exclude=[
+            "*.tests",
+            "*.tests.*"
+            "tests.*",
+            "tests",
+            "test"
+        ]
+    ),
+    install_requires=REQUIRES,
+    extras_require={
+        'dev': REQUIRES_DEV,
+    },
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
